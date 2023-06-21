0f4a27a10fa9ba284dd8e9b2191219c997ea8521
Aishwary Jain <118094105+AishwaryJain07@users.noreply.github.com>
2023-06-21 15:46:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+}
+
+
+
+
+  function toggleMenu() {
+    var menuItems = document.querySelector(".navbar .menu-items");
+    menuItems.classList.toggle("show");
+  }
